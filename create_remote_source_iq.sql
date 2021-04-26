5f584a178dda4f9ccaacdc5b653b64cb0304125e
Euiman KIm <74029470+euimankim@users.noreply.github.com>
2021-04-26 16:25:20 +0900

    Create create_remote_source_iq.sql

---
+++ create_remote_source_iq.sql
@@ -0,0 +1,4 @@
+CREATE REMOTE SOURCE MY_IQ3 ADAPTER iqodbc 
+    CONFIGURATION 'Driver=libdbodbc17_r.so;host=b912efae-c19c-41a2-8a6e-ea26bdc0158d.iq.hdl.trial-us10.hanacloud.ondemand.com:443;ENC=TLS(tls_type=rsa;direct=yes)'
+    WITH CREDENTIAL TYPE 'PASSWORD' USING 'user=HDLADMIN;password=XXXXXXX';
+    
